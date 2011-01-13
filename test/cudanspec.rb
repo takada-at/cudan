e638d8659b46221d7ad1c30a869bddf86babd12b
takada-at <takada-at@klab.org>
2011-01-13 17:28:58 +0900

    create

--- test/cudanspec.rb
+++ test/cudanspec.rb
@@ -23,16 +23,29 @@ WW::Server[:server] ||= WW::Server::build_double(port) do
     }
 end
 WW::Server.start_once
-describe Cudan, 'request for localhost' do
-    it 'test with regexp' do
+describe Cudan::RegexpCudan, 'request for localhost' do
+    it 'should raise no error with regexp' do
         url = baseurl + '/regexp'
         o = Cudan::RegexpCudan::new
-        o.execute('<p class=\'hoge\'>(.*)</p>', 'abcdefg', url)
+        expect{
+             o.execute('<p class=\'hoge\'>(.*)</p>', 'abcdefg', url)
+        }.to_not raise_error(nil)
     end
-    it 'test with xpath' do
+end
+describe Cudan::XpathCudan, 'request for localhost' do
+    it 'should raise no error with xpath' do
         url = baseurl + '/xpath'
         o = Cudan::XpathCudan::new
-        o.execute('//p[@class="hoge"]/text()', 'abcdefg', url)
-        o.execute('//p[@class="hoge"]', 'abcdefg', url)
+        expect{
+            o.execute('//p[@class="hoge"]/text()', 'abcdefg', url)
+        }.to_not raise_error(nil)
+        expect{
+            o.execute('//p[@class="hoge"]', 'abcdefg', url)
+        }.to_not raise_error(nil)
+    end
+    it 'should raise error with xpath' do
+        url = baseurl + '/xpath'
+        o = Cudan::XpathCudan::new
+        expect{ o.execute('//p[@class="fuga"]', 'hoge', url) }.to raise_error(SystemExit)
     end
 end
